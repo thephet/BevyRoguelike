22b534b8a826844f13e4f2fe867070d11e1ebc3e
Juan Manuel Parrilla Gutiérrez <juanma@juans-air.home>
2021-12-27 16:30:38 +0000

    still working on ui

--- src/hud.rs
+++ src/hud.rs
@@ -5,40 +5,52 @@ fn setup_ui(
     asset_server: ResMut<AssetServer>,
     mut color_materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let font: Handle<Font> = asset_server.load("fonts/Schrodinger.ttf");
+    let font: Handle<Font> = asset_server.load("fonts/dos.ttf");
     let material = color_materials.add(Color::NONE.into());
 
     commands
-        // Time text node
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    left: Val::Px(10.),
-                    top: Val::Px(10.),
+    .spawn_bundle(NodeBundle {
+        style: Style {
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            border: Rect::all(Val::Px(5.0)),
+            ..Default::default()
+        },
+        material: color_materials.add(Color::rgb(0.65, 0.65, 0.65).into()),
+        ..Default::default()
+    })
+    .with_children(|parent| {
+        // left vertical fill (content)
+        parent
+            .spawn_bundle(NodeBundle {
+                style: Style {
+                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                    align_items: AlignItems::FlexEnd,
                     ..Default::default()
                 },
+                material: color_materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
                 ..Default::default()
-            },
-            material: material.clone(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent
-            .spawn_bundle(TextBundle {
-                text: Text::with_section(
-                    "Time: 0.0",
-                    TextStyle {
-                        font_size: 40.0,
-                        font: font.clone(),
-                        color: Color::rgb(0.9, 0.9, 0.9),
+            })
+            .with_children(|parent| {
+                // text
+                parent.spawn_bundle(TextBundle {
+                    style: Style {
+                        margin: Rect::all(Val::Px(5.0)),
+                        ..Default::default()
                     },
-                    Default::default(),
-                ),
+                    text: Text::with_section(
+                        "Text Example",
+                        TextStyle {
+                            font: asset_server.load("fonts/dos.ttf"),
+                            font_size: 30.0,
+                            color: Color::WHITE,
+                        },
+                        Default::default(),
+                    ),
                     ..Default::default()
-                })
-                .insert(TimeText);
+                });
+            });
         });
+
 }
 
 struct TimeText;
