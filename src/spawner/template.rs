444686056a038b7cffafa5ab963da99e458f6720
Juanma <juanma@home>
2022-04-29 14:27:54 +0100

    working on template spawner

--- src/spawner/template.rs
+++ src/spawner/template.rs
@@ -26,9 +26,35 @@ pub struct Templates {
 }
 
 impl Templates {
-    pub fn load() -> Self {
+
+    pub fn load() -> Self 
+    {
         let file = File::open("assets/template.ron")
             .expect("Failed opening file.");
         from_reader(file).expect("Unable to load templates.")
+    }
+
+    pub fn spawn_entities(
+        &self,
+        level: usize,
+        spawn_points: &[Position]
+    ) {
+        let mut rng = rand::thread_rng();
+
+        let mut available_entities = Vec::new();
+        self.entities.iter()
+            .filter(|e| e.levels.contains(&level))
+            .for_each(|t| {
+                for _ in 0 .. t.frequency {
+                    available_entities.push(t);
+                }
+            });
+
+        spawn_points.iter().for_each(|pt| {
+            let target_index = rng.gen_range(0..available_entities.len());
+            if let entity = available_entities[target_index].clone() {
+                self.spawn_entity(pt, entity);
+            }
+        })
     }
 }
